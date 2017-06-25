eda137a05fb9ca85956f0048d4ac727ed71dd052
jtaaa <joshuatallum@gmail.com>
2017-06-24 21:31:40 -0400

    Use Luis intents to handle root dialog

--- TestingBotApp/Dialogs/RootDialog.cs
+++ TestingBotApp/Dialogs/RootDialog.cs
@@ -2,30 +2,39 @@
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
+using System.Threading;
+using System.Text.RegularExpressions;
+using System.Linq;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
 
 namespace TestingBotApp.Dialogs
 {
+    [LuisModel("05910c2c-3cc3-4cff-b1d0-98e7b0ee630e", "961ddeee452f49a1869de016ac3f0a4d")]
     [Serializable]
-    public class RootDialog : IDialog<object>
+    public class RootDialog : LuisDialog<object>
     {
-        public Task StartAsync(IDialogContext context)
-        {
-            context.Wait(MessageReceivedAsync);
 
-            return Task.CompletedTask;
+        [LuisIntent("None")]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            string message = $"Sorry I did not understand: " + string.Join(", ", result.Intents.Select(i => i.Intent));
+            await context.PostAsync(message);
+            context.Wait(MessageReceived);
         }
 
-        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        [LuisIntent("GetAlias")]
+        public async Task GetAlias(IDialogContext context, LuisResult result)
         {
-            var activity = await result as Activity;
+            EntityRecommendation title;
+            string reply = "";
+            if (result.TryFindEntity("Name", out title))
+            {
+                reply = title.Entity;
+            }
 
-            // calculate something for us to return
-            int length = (activity.Text ?? string.Empty).Length;
-
-            // return our reply to the user
-            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
-
-            context.Wait(MessageReceivedAsync);
+            await context.PostAsync(reply);
+            context.Wait(MessageReceived);
         }
     }
 }
